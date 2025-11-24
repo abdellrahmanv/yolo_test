Fix input preprocessing - use float32 normalization for quantized model

tensorlite/scripts/run_best_custom.py
@@ -93,8 +93,7 @@ def run_test():
             
             frame_count += 1
             
-            img = cv2.resize(frame, (IMG_SIZE, IMG_SIZE))
-            img = np.expand_dims(img, axis=0).astype(np.uint8)
+            img = cv2.resize(frame, (IMG_SIZE, IMG_SIZE))\n            img = img.astype(np.float32) / 255.0\n            img = np.expand_dims(img, axis=0)
             
             t0 = time.time()
             interpreter.set_tensor(input_details[0]['index'], img)